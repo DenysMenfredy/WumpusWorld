5d6000e58060f4695a62e889b7136acc057e67bc
Renua-Meireles <renua.meireles@gmail.com>
2019-10-21 20:38:45 -0300

    optimization made,comments and scream handle added

--- agent_function.py
+++ agent_function.py
@@ -4,107 +4,113 @@ from random import choice
 
 def agentFunction(state:dict) -> None:
     size:int                = state['size']
-    x, y                    = state['coordenates']
+    x, y                    = state['coordinates']
     perceptions:list(str)   = state['perceptions']
     arrow:bool              = state['arrow']
 
-    # first column
+    """
+        Tratamento das sensacoes por prioridade: BRILHO > GRITO > FEDOR > BRISA > NADA.
+            Brilho -> Pegar (nao importa mais nada!, apenas pegue aquele bendito ouro!).
+            Grito -> Mover (wumpus ja esta morto, apenas se move).
+            Fedor -> Atirar/Mover (wumpus pode estar vivo ou morto, tenta atirar).
+            Brisa -> Mover (nao há nada para se fazer, apenas mover)
+            Nada -> Mover (avante!).
+    """
+
+    if ('glitter' in perceptions): return Action('pickup')
+
+    # primeira coluna
     if y == 0:
-        # first line
+        # primeira linha
         if x == 0:
-            if 'shine' in perceptions: return Action('pickup')
-
-            elif ('breeze' in perceptions) and ('stench' in perceptions):
-                actionName = 'move' if not arrow else choice(['move', 'shoot'])
-                return Action(actionName, direction = choice(['N','L']))
-                
-            elif ('breeze' in perceptions):
-                return Action('move', direction = choice(['N','L']))
+            if ('scream' in perceptions): return Action('move', direction = choice(['N','L']))
 
             elif ('stench' in perceptions):
-                actionName = 'move' if not arrow else choice(['move', 'shoot'])
+                actionName = 'move' if not arrow else 'shoot'
                 return Action(actionName, direction = choice(['N','L']))
                 
-        # last line
+            elif ('breeze' in perceptions): return Action('move', direction = choice(['N','L']))
+
+            else: return Action('move', direction = choice(['N','L']))
+
+        # ultima linha
         elif x == size-1:
-            if 'shine' in perceptions: return Action('pickup')
-
-            elif ('breeze' in perceptions) and ('stench' in perceptions):
-                actionName = 'move' if not arrow else choice(['move', 'shoot'])
-                return Action(actionName, direction = choice(['S','L']))
-                
-            elif ('breeze' in perceptions):
-                return Action('move', direction = choice(['S','L']))
+            if ('scream' in perceptions): return Action('move', direction = choice(['S','L']))
 
             elif ('stench' in perceptions):
-                actionName = 'move' if not arrow else choice(['move', 'shoot'])
+                actionName = 'move' if not arrow else 'shoot'
                 return Action(actionName, direction = choice(['S','L']))
+                
+            elif ('breeze' in perceptions): return Action('move', direction = choice(['S','L']))
 
-        # midle lines
+            else: return Action('move', direction = choice(['S','L']))
+
+        # linhas do meio
         else:
-            if 'shine' in perceptions: return Action('pickup')
-
-            elif ('breeze' in perceptions) and ('stench' in perceptions):
-                actionName = 'move' if not arrow else choice(['move', 'shoot'])
-                return Action(actionName, direction = choice(['N','L','S']))
-                
-            elif ('breeze' in perceptions):
-                return Action('move', direction = choice(['N','L','S']))
+            if ('scream' in perceptions): return Action('move', direction = choice(['N','L','S']))
 
             elif ('stench' in perceptions):
-                actionName = 'move' if not arrow else choice(['move', 'shoot'])
+                actionName = 'move' if not arrow else 'shoot'
                 return Action(actionName, direction = choice(['N','L','S']))
+                
+            elif ('breeze' in perceptions): return Action('move', direction = choice(['N','L','S']))
 
-    # last column
+            else: return Action('move', direction = choice(['N','L','S']))
+
+    # ultima coluna
     elif y == size-1:
-        # first line
+        # primeira linha
         if x == 0:
-            if 'shine' in perceptions: return Action('pickup')
-            elif ('breeze' in perceptions) and ('stench' in perceptions):
-                actionName = 'move' if not arrow else choice(['move','shoot'])
-                return Action(actionName, direction = choice(['O','S']))
-            elif ('breeze' in perceptions):
-                return Action('move', direction = choice(['O','S']))
+            if ('scream' in perceptions): return Action('move', direction = choice(['O','S']))
+
             elif ('stench' in perceptions):
                 actionName = 'move' if not arrow else choice(['move','shoot'])
                 return Action(actionName, direction = choice(['O','S']))
-        # last line
+
+            elif ('breeze' in perceptions): return Action('move', direction = choice(['O','S']))
+
+            else: return Action('move', direction = choice(['O','S']))
+
+        # ultima linha
         elif x == size-1:
-            if 'shine' in perceptions: return Action('pickup')
-            elif ('breeze' in perceptions) and ('stench' in perceptions):
-                actionName = 'move' if not arrow else choice(['move','shoot'])
-                return Action(actionName, direction = choice(['O','N']))
-            elif ('breeze' in perceptions):
-                return Action('move', direction = choice(['O','N']))
+            if ('scream' in perceptions): return Action('move', direction = choice(['O','N']))
+
             elif ('stench' in perceptions):
                 actionName = 'move' if not arrow else choice(['move','shoot'])
                 return Action(actionName, direction = choice(['O','N']))
-        # midle lines
+
+            elif ('breeze' in perceptions): return Action('move', direction = choice(['O','N']))
+
+            else: return Action('move', direction = choice(['O','N']))
+
+        # linhas do meio
         else:
-            if 'shine' in perceptions: return Action('pickup')
-            elif ('breeze' in perceptions) and ('stench' in perceptions):
-                actionName = 'move' if not arrow else choice(['move','shoot'])
-                return Action(actionName, direction = choice(['N','O','S']))
-            elif ('breeze' in perceptions):
-                return Action('move', direction = choice(['N','O','S']))
+            if ('scream' in perceptions): return Action('move', direction = choice(['N','O','S']))
+
             elif ('stench' in perceptions):
                 actionName = 'move' if not arrow else choice(['move','shoot'])
                 return Action(actionName, direction = choice(['N','O','S']))
 
-    # midle columns
+            elif ('breeze' in perceptions): return Action('move', direction = choice(['N','O','S']))
+
+            else: return Action('move', direction = choice(['N','O','S']))
+
+    # colunas do meio
     else:
-        # first line
+        # primeira linha
         if x == 0:
-            if 'shine' in perceptions: return Action('pickup')
-            elif ('breeze' in perceptions) and ('stench' in perceptions):
-                pass
-            elif ('breeze' in perceptions):
-                pass
-            elif ('stench' in perceptions):
-                pass
-        # last line
+            if ('scream' in perceptions): pass
+
+            elif ('stench' in perceptions): pass
+
+            elif ('breeze' in perceptions): pass
+
+            elif ('stench' in perceptions): pass
+
+            else: return Action('move', direction = choice([]))
+        # ultima linha
         elif x == size-1:
             pass
-        # midle lines
+        # linhas do meio
         else:
             pass
